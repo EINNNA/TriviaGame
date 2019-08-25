f2591194e618259cb893f51eb99de97e8365db3e
EINNNA <annie_t_@hotmail.com>
2019-08-25 23:41:37 +1000

    ideas

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+// what is blind baking: blind person baking, baking a blind person, baking without looking in the oven, //baking without a filling//
+//which flour has the nighest amount of starch? starch flour, all purpose flour, //cake flour//, almond flour
+//what is a good substitute for eggs? milk, //applesauce//, mayonaisse, custard
+//red velvet is which type of flavour? strawberry, raspberry, //chocolate// vanilla
+//what type of cheese is used in cheesecake? brie cheese, cheddar cheese, //cream cheese//, goats cheese
+//who started the tradition of putting candles on birthday cakes? ur mom, //greeks// victorians, romans
+//
